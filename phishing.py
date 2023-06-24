4db23751cdd55cb8ea1c7385830101346ac8a987
SYLVERCYBER-ORACLE <113990831+SYLVERCYBER-EXPERT@users.noreply.github.com>
2023-06-24 16:54:26 +0100

    Update phishing.py

--- phishing.py
+++ phishing.py
@@ -84,12 +84,3 @@ if __name__ == '__main__':
 	except AttributeError:
 		print("Error. Please provide the command line argument before running") 
 
-
-#parameters: smtp.sendgrid.net 25 apikey SG.uc.rccl-RRGmyALisacnjeklknzcjksdclknd test_email.html https://www.gmail.com "Contract  Approval" "Attacker@gmail.com" "vitim@gmail.com"
-#smtp.sendgrid.net is mailing server, in the above example we are using sendgrid.net. 25 is the port number.
-#apikey is generated from the mailing server, in the above exaple it was generated from sendgrid.net
-#attack your html file that contain your message, in the above example the file name is test_email.html 
-#Enter to webpage you will like the vitim to be redirected to after clicking the your phishing link
-#enter Email subject, in the above example the subject is " Contract approval"
-# Next is the Attacker email address in ""
-#Finally, the vitim email address in ""
